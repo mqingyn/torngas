a1529a4f1084c753eba30b773adfc8da0edcf2f2
mqingyn <mqingyn@gmail.com>
2013-07-13 17:31:21 +0800

    modified runserver.py

--- torngas/resource/app/runserver.py
+++ torngas/resource/app/runserver.py
@@ -5,9 +5,8 @@ from torngas.initserver import Server
 
 PROJECT_NAME = "app"
 PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
-os.environ.setdefault("TORNGAS_ONLINE_SETTINGS_MODULE", "%s.settings_production" % PROJECT_NAME)
-os.environ.setdefault("TORNGAS_DEV_SETTINGS_MODULE", "%s.settings_devel" % PROJECT_NAME)
-os.environ.setdefault("TORNGAS_TEST_SETTINGS_MODULE", "%s.settings_functest" % PROJECT_NAME)
+os.environ.setdefault("TORNGAS_PROJECT_NAME", PROJECT_NAME)
+
 if __name__ == '__main__':
     server = Server(PROJECT_PATH, application=None)
     server.load_urls()
