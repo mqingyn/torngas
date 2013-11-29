ea3b754659c84e687fdc14b5c25283b475153e04
mqingyn <mqingyn@gmail.com>
2013-11-29 11:04:05 +0800

    修正示例程序的参数错误

--- torngas/resource/app/Main/urls.py
+++ torngas/resource/app/Main/urls.py
@@ -1,7 +1,7 @@
 #-*-coding=utf8-*-
 from torngas.helpers.route_helper import url, RouteLoader
 
-route = RouteLoader(path_prefix='Main.views', path='/', app_name='Main')
+route = RouteLoader(path_prefix='Main.views', path='/', app_folder='Main')
 
 urls = route.urlhelper(
     url('Index', r'/', 'view=main_handler,handler=Main')
