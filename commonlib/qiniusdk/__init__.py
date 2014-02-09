75ce5645b1f1d55b766cf86a7d0eb221d099bc72
mqingyn <mqingyn@gmail.com>
2014-02-09 23:11:59 +0800

    add

---
+++ commonlib/qiniusdk/__init__.py
@@ -0,0 +1,10 @@
+'''
+Qiniu Resource Storage SDK for Python
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+For detailed document, please see:
+<https://github.com/qiniu/python-sdk/blob/develop/docs/README.md>
+'''
+
+# -*- coding: utf-8 -*-
+__version__ = '6.1.3'
---
+++ commonlib/qiniusdk/conf.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+ACCESS_KEY = ""
+SECRET_KEY = ""
+
+RS_HOST = "rs.qbox.me"
+RSF_HOST = "rsf.qbox.me"
+UP_HOST = "up.qiniu.com"
+
+from . import __version__
+USER_AGENT = "qiniu python-sdk v%s" % __version__
