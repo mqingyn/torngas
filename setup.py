2061ead1a3ffeb76f275a1ee465de537c2897ffa
mqingyn <mqingyn@gmail.com>
2014-02-11 23:17:42 +0800

    改善配置

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="torngas",
-    version="0.2.3",
+    version="0.2.4",
     description="torngas based on tornado",
     author="qingyun meng",
     url="http://github.com/mqingyn/torngas",
--- torngas/__init__.py
+++ torngas/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 __author__ = 'mengqingyun'
-__version__ = '0.2.3'
+__version__ = '0.2.4'
 
 version = tuple(map(int, __version__.split('.')))
