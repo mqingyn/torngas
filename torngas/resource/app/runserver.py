ecc9406e93818221e86cef28f11d7a3b3da8e907
mengqingyun <mengqingyun@zhangyue.com>
2014-02-13 13:52:28 +0800

    优化结构

--- torngas/resource/app/runserver.py
+++ torngas/resource/app/runserver.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import os
-from torngas.webserver import Server
+from torngas import webserver
 
 PROJECT_OATH = os.path.dirname(os.path.abspath(__file__))
 if __name__ == '__main__':
-    server = Server()
+    server = webserver.Server()
     server.init(PROJECT_OATH, application=None)
     server.load_urls()
     server.load_application()
