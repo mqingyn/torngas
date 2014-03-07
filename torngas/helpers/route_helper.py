d2befaaf3ab557749f658232c8d9f3ba33d5253f
mqingyn <mqingyn@gmail.com>
2014-03-07 16:43:34 +0800

    改进urlhelper

--- torngas/helpers/route_helper.py
+++ torngas/helpers/route_helper.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+import inspect
 from torngas import exception
 from tornado.web import url as urlspec
 from tornado.util import import_object
@@ -10,7 +12,7 @@ class RouteLoader(object):
     路由加载器，将路由加载进tornado的路由系统中
     path:由于设计为子应用形式，路由最终路径为 /path/你的路由，比如blog应用下的/index,会被解析为/blog/index,
         如果不希望在路由前加/path，则为单个路由设置path='/'，path为必填参数
-    app_name:设置为子应用的模块名，大小写必须相同，必填
+    subapp_name:设置为子应用的模块名，大小写必须相同，必填
     """
 
     def __init__(self, path=None, subapp_name=None):
@@ -21,13 +23,18 @@ class RouteLoader(object):
         self.path = path if path != '/' else ''
         self.subapp_name = subapp_name
 
-    def urlhelper(self, *urllist):
+    def urlhelper(self, prefix='', *urllist):
         """
         路由列表list
         """
         urls = []
         for u in urllist:
-            handler_module = '.'.join([self.subapp_name, u.get('handler_module', '')])
+            handler = u.get('handler_module', '')
+            if isinstance(handler, basestring):
+                handler_module = '.'.join([prefix, u.get('handler_module', '')])
+                handler_module = import_object(handler_module)
+            else:
+                handler_module = handler
             pattern = u.get('pattern')
             pattern += '?' if pattern.endswith('/') else '/?'
             path = u.get('path', None)
@@ -42,7 +49,7 @@ class RouteLoader(object):
             kw['subapp_name'] = self.subapp_name
             url_name = ''.join([self.subapp_name, '-', u.get('name')])
 
-            url=urlspec(pattern, import_object(handler_module), kwargs=kw, name=url_name)
+            url = urlspec(pattern, handler_module, kwargs=kw, name=url_name)
             urls.append(url)
 
         return urls
@@ -53,17 +60,27 @@ class url(object):
 
     :param name:路由的名字，设计为必填项。这样能更好的管理路由，方便使用reverse_url生成路由
     :param pattern:路由表达式
-    :param process_setting:路由的handler，view，path设置
+    :param view:路由的handler，可为字符串或者view class
     :param kwargs:额外参数提供
+    :param path:path:由于设计为子应用形式，路由最终路径为 /path/你的路由，比如blog应用下的/index,会被解析为/blog/index,
+        如果不希望在路由前加/path，则为单个路由设置path='/'，path为必填参数
     :return:dict，路由字典
     """
 
-    def __call__(self, name=None, pattern=None, handler_module='', path=None, kwargs=None):
+    def __call__(self, name=None, pattern=None, view='', path=None, kwargs=None):
+
+
+        if not name:
+            raise exception.ArgumentError('You must give a value for "name"')
         if not kwargs:
             kwargs = {}
+        if not view:
+            raise exception.ArgumentError('You must give a value for "view"')
+
+
         return dict(
             pattern=pattern,
-            handler_module=handler_module,
+            handler_module=view,
             name=name,
             path=path,
             kwargs=kwargs
--- torngas/resource/app/Main/urls.py
+++ torngas/resource/app/Main/urls.py
@@ -3,8 +3,8 @@ from torngas.helpers.route_helper import url, RouteLoader
 from . import SUBAPP_NAME
 route = RouteLoader(path='/', subapp_name='Main')
 
-urls = route.urlhelper(
-    url('Index', r'/', 'views.main_handler.Main')
+urls = route.urlhelper('views',
+    url('Index', r'/', 'main_handler.Main')
 )
 
 if __name__ == '__main__':
