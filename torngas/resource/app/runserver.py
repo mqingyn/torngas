e6a60b25ce235136d31bbd5485a312d4f08d06c5
qingyun.meng <mqingyn@gmail.com>
2014-04-03 17:02:32 +0800

    Update runserver.py

--- torngas/resource/app/runserver.py
+++ torngas/resource/app/runserver.py
@@ -6,9 +6,6 @@ from torngas import webserver
 
 PROJECT_OATH = os.path.dirname(os.path.abspath(__file__))
 if __name__ == '__main__':
-    server = webserver.Server()
-    server.init(PROJECT_OATH, application=None)
-    server.load_urls()
-    server.load_application()
-    server.server_start()
+    os.environ.setdefault("SETTINGS_MODULE", "settings.setting")
+    Server().runserver(PROJECT_OATH,None)
 
