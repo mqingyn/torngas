da3e94526f48e76a6c648dcc60a72252453632ce
mqingyn <mqingyn@gmail.com>
2014-04-04 17:24:19 +0800

    update demo

---
+++ demo/runserver.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from torngas.webserver import Server
+
+PROJECT_OATH = os.path.dirname(os.path.abspath(__file__))
+if __name__ == '__main__':
+    os.environ.setdefault("SETTINGS_MODULE", "settings.setting")
+    Server().runserver(PROJECT_OATH,None)
+
