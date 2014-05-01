d9963512be3f17012c94bb6721a76552ecccb874
qingyun.meng <mqingyn@gmail.com>
2014-05-01 13:11:01 +0800

    clean

--- demo/helloworld/urls.py
+++ demo/helloworld/urls.py
@@ -5,7 +5,8 @@ from torngas.helpers.route_helper import url, RouteLoader
 route = RouteLoader(path='/')
 
 urls = route.urlhelper('helloworld.views',
-    url('Index', r'/', 'main_handler.Main') #也可以传递对象
+                       #也可以传递对象
+                       url('Index', r'/', 'main_handler.Main')
 )
 
 if __name__ == '__main__':
