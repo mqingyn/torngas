ae1e90cf618a50f132ccccca4c3563368912eed4
qingyun.meng <mqingyn@gmail.com>
2014-08-14 17:38:01 +0800

    完整支持yield语法，不在局限于Task的方式，不再需要绑定空callback

--- torngas/decorators/async_execute.py
+++ torngas/decorators/async_execute.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 
 """
-线程future
+线程future，此函数依赖tornado版本>3.0
 如果某些函数耗时较长，且不提供异步回调的功能，例如同步调用mysql，执行一个耗时命令并返回结果，
 这些函数无法利用tornado的async特性，此处利用线程池模拟了一个异步方法，可以防止同步耗时操作阻塞进程，
-只要在需要异步的方法上加@async_execute装饰器，并且函数最后一个参数名为callback，则该函数便可以支持
-@asynchronous或gen.Task的非阻塞方式调用
+只要在需要异步的方法上加@async_execute装饰器，则该函数便可以支持
+@asynchronous或gen的非阻塞方式调用
 
 依赖：该方法必须安装futures模块才可以使用
 例子：
@@ -17,11 +17,11 @@ class Test(Base):
         a=''
         b=''
         #支持使用gen的task模块来同步化异步调用
-        result = yield Task(self.dosomething, a, b)
+        result = yield self.dosomething(a, b)
         self.finish(result)
 
     @async_execute
-    def dosomething(self,a,b,callback=None):
+    def dosomething(self,a,b):
         # 这里可能耗时很久
         #callback参数不会在这里有任何调用，这个耗时方法本身并没有异步，
         #同步的方法无论如何都不会毫无代价的变成异步，
@@ -49,35 +49,28 @@ class Test(Base):
         return result
 """
 
+
 import functools
 from tornado.netutil import Resolver, ThreadedResolver
 from tornado.ioloop import IOLoop
 
-Resolver.configure('tornado.netutil.ThreadedResolver',
-                   num_threads=10)
+Resolver.configure('tornado.netutil.ThreadedResolver', num_threads=30)
 
-from torngas.exception import ArgumentError
+def async_execute(fn):
+    """
+    新版tornado已有这个函数的实现，但是需要在class中绑定self.ioloop和self.executor,不太方便，这个版本消除了这个问题
+    """
+    thread_resolver = ThreadedResolver()
 
-
-def async_execute(method):
-    @functools.wraps(method)
-    def decor(*args, **kwargs):
-        def wrapper(callback):
-            thread_resolver = ThreadedResolver()
-            fut = thread_resolver.executor.submit(method, *args, **kwargs)
-            fut.add_done_callback(
-                lambda future: IOLoop.current().add_callback(functools.partial(callback, future.result())))
-
-        callback = kwargs.get('callback', None)
-
-        if not callback:
-            callback = args[-1]
-        if not callable(callback):
-            raise ArgumentError("The last parameter named 'callback' must be a callable function.")
-
-        return wrapper(callback)
-
-    return decor
+    @functools.wraps(fn)
+    def wrapper(self, *args, **kwargs):
+        callback = kwargs.pop("callback", None)
+        future = thread_resolver.executor.submit(fn, self, *args, **kwargs)
+        if callback:
+            IOLoop.current().add_future(future,
+                                    lambda future: callback(future.result()))
+        return future
+    return wrapper
 
 
 if __name__ == '__main__':
