8fa224ef97ec3f6307ff57badc1b840944c9fecd
qingyun.meng <qingyun.meng@autonavi.com>
2015-01-14 20:37:11 +0800

    优化模块加载

--- torngas/__init__.py
+++ torngas/__init__.py
@@ -8,7 +8,6 @@ version = tuple(map(int, __version__.split('.')))
 from settings_manager import settings
 from webserver import Server, run
 from exception import ConfigError, ArgumentError
-from mixins.flash_message import FlashMessageMixIn
 from urlhelper import Url, route, include
 from utils import is_future, RWLock, cached_property, lazyimport, Null, \
     safestr, safeunicode, strips, iterbetter
