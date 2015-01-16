c22262871f9b40e192ee65e3c7f28db2771e1784
qingyun.meng <qingyun.meng@autonavi.com>
2015-01-16 19:12:14 +0800

    重写signal模块，采用django1.7 signal模块实现。

--- torngas/dispatch/signals.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from torngas.dispatch import Signal
-
-call_started = Signal()
-handler_started = Signal()
-handler_finished = Signal()
-call_finished = Signal()
-# got_request_exception = Signal(providing_args=["request"])
---
+++ torngas/signal.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8  -*-
+# Created by mqingyn on 2015/1/16.
+from dispatch import Signal
+
+call_started = Signal(providing_args=["request"])
+handler_started = Signal(providing_args=["handler"])
+handler_response = Signal(providing_args=["handler", "chunk"])
+call_finished = Signal(providing_args=["handler"])
+handler_render = Signal(providing_args=["handler", "template_name", "kwargs"])
+app_start = Signal(providing_args=["app"])
