93541b031ea46844e7da4027bdc7a9aa958cf007
qingyun.meng <mqingyn@gmail.com>
2015-01-25 14:09:19 +0800

    增加gunicorn 配置文件实例

---
+++ demo/run_gunicorn.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from torngas.webserver import Server
+
+
+"""
+run at gunicorn.
+gunicorn -c gunicorn.py.conf run_gunicorn:app
+torngas settings 写在gunicorn.conf.py中：
+os.environ.setdefault('TORNGAS_APP_SETTINGS', 'settings.setting')
+"""
+server = Server()
+server.parse_logger()
+server.load_urls()
+
+app = server.load_application()
+
+
