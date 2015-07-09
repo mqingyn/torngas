00410e0370ea0cddab19fde161b36f3ddb66ee28
qingyun.meng <qingyun.meng@alibaba-inc.com>
2015-07-09 22:57:45 +0800

    整理accesslog输出

--- torngas/middleware/accesslog.py
+++ torngas/middleware/accesslog.py
@@ -10,36 +10,21 @@ import logging
 access_log = logging.getLogger('torngas.accesslog')
 
 
-def log_request(handler):
-    _datetime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    request_time = 1000.0 * handler.request.request_time()
-    user_agent = handler.request.headers.get("User-Agent", "-")
-    remote_ip = handler.request.remote_ip
-    uri = handler.request.uri
-    method = handler.request.method
-    version = handler.request.version
-    status = handler.get_status()
-    referer = handler.request.headers.get('Referer', "-")
-    content_length = handler._headers.get("Content-Length", "-")
-    _message = '%s - - [%s] "%s %s %s" %s %s "%s" "%s" %dms' % (
-        remote_ip,
-        _datetime,
-        method,
-        uri,
-        version,
-        status,
-        content_length,
-        referer,
-        user_agent,
-        request_time
-    )
-    access_log.info(_message)
-
-
 class AccessLogMiddleware(object):
     def process_init(self, application):
         application.settings['log_function'] = lambda _: None
 
     def process_endcall(self, handler, clear):
-        log_request(handler)
-
+        message = '%s - - [%s] "%s %s %s" %s %s "%s" "%s" %dms' % (
+            handler.request.remote_ip,
+            datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            handler.request.method,
+            handler.request.uri,
+            handler.request.version,
+            handler.get_status(),
+            handler.request.headers.get("Content-Length", "-"),
+            handler.request.headers.get('Referer', "-"),
+            handler.request.headers.get("User-Agent", "-"),
+            1000.0 * handler.request.request_time()
+        )
+        access_log.info(message)
