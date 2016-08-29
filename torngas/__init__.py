8c287ca7b3f184c692356c81a93007936a7a5b01
素澈 <qingyun.meng@alibaba-inc.com>
2016-08-29 12:38:37 +0800

    fix import torngas but not found tornado

--- torngas/__init__.py
+++ torngas/__init__.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 __author__ = 'mqingyn'
-__version__ = '1.8.1'
+__version__ = '1.8.2'
 
 version = tuple(map(int, __version__.split('.')))
-
-from settings_manager import settings
-from webserver import Server, run
-from exception import ConfigError, ArgumentError
-from urlhelper import Url, route, include
-from utils import is_future, RWLock, cached_property, lazyimport, Null, \
-    safestr, safeunicode, strips, iterbetter, sleep, request_context
-from storage import storage, storify, sorteddict, ThreadedDict
+try:
+    from settings_manager import settings
+    from webserver import Server, run
+    from exception import ConfigError, ArgumentError
+    from urlhelper import Url, route, include
+    from utils import is_future, RWLock, cached_property, lazyimport, Null, \
+        safestr, safeunicode, strips, iterbetter, sleep, request_context
+    from storage import storage, storify, sorteddict, ThreadedDict
+except:
+    pass
